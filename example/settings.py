71e3e0d1faa27367ff251b9fd686dd971bd4d8f9
Daniele Procida <daniele@vurt.org>
2011-07-02 12:39:03 +0100

    settings file

--- example/settings.py
+++ example/settings.py
@@ -17,11 +17,11 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'arkestra_medic_dev',                      # Or path to database file if using sqlite3.
-        'USER': 'arkestra',                      # Not used with sqlite3.
-        'PASSWORD': 'avuzritfanu',                  # Not used with sqlite3.
-        'HOST': '/tmp/mysql.sock',                      # Set to empty string for localhost. Not used with sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'example.db',                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
