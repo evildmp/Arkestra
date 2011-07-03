ef5e27852044ec35d512a6ade25f90d4a43aedf0
Daniele Procida <daniele@vurt.org>
2011-07-03 23:35:36 +0100

    arkestra_image_plugin isn't ready for the world yet

--- arkestra_image_plugin/admin.py
+++
@@ -1,7 +0,0 @@
-# from django.contrib import admin
-# from models import ThumbnailOption
-# 
-# class ThumbnailOptionAdmin(admin.ModelAdmin):
-#     list_display = ('name', 'width', 'height')
-# 
-# admin.site.register(ThumbnailOption, ThumbnailOptionAdmin)
