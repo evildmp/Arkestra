f44edb0894021202ca4b40a8b954fe25c2ad0cd0
Daniele Procida <daniele@vurt.org>
2011-08-16 15:57:21 +0100

    behaves more felxibly for hosting entities

--- vacancies_and_studentships/views.py
+++ vacancies_and_studentships/views.py
@@ -153,9 +153,9 @@ def all_current_studentships(request, slug=getattr(default_entity, "slug", None)
 
 def vacancy_and_studentship(item):
     item.links = object_links(item) # not needed if using get_links templatetag
-    item.hosted_by = item.hosted_by or default_entity
-    item.link_to_vacancies_and_studentships_page = item.hosted_by.get_related_info_page_url("vacancies-and-studentships")
-    item.template = item.hosted_by.get_template()
+    entity = item.hosted_by or default_entity
+    item.link_to_vacancies_and_studentships_page = entity.get_related_info_page_url("vacancies-and-studentships")
+    item.template = entity.get_template()
     return item
 
 def vacancy(request, slug):
