98b91133cedd156584e133180f41dd1c14955a2d
Daniele Procida <daniele@apple-juice.co.uk>
2011-09-11 00:11:33 +0100

    settings for example project tweaked

--- example/arkestra_settings.py
+++ example/arkestra_settings.py
@@ -8,6 +8,14 @@
 # must match the id of your base or default entity
 ARKESTRA_BASE_ENTITY = 1 
 
+# MULTIPLE_ENTITY_MODE is for projects hosting the site of more than one entity
+# This does not necessarily entail a site for complex organisation,
+# or for a number of different organisations - being able to redirect 
+# news and events items to particular entities for example requires
+# MULTIPLE_ENTITY_MODE to be True 
+
+MULTIPLE_ENTITY_MODE = True
+
 # ------------------------ Semantic editor
 
 import os
