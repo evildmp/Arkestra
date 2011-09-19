b72d920b3467b5f3bdc9d51cd2319684b54f96fc
Daniele Procida <daniele@apple-juice.co.uk>
2011-09-19 11:09:39 +0100

    search uses summary fo metadata now

--- news_and_events/link_schemas.py
+++ news_and_events/link_schemas.py
@@ -4,10 +4,10 @@ from news_and_events import models, admin
 from links import schema, LinkWrapper
 
 schema.register(models.NewsArticle, search_fields=admin.NewsArticleAdmin.search_fields,
-    metadata='subtitle', heading='"Related news"',
-    short_text = 'short_title', description='subtitle',
+    metadata='summary', heading='"Related news"',
+    short_text = 'short_title', description='summary',
     )
 schema.register(models.Event, search_fields=admin.EventAdmin.search_fields, 
-    metadata='subtitle', heading='"Related events"',
-    short_text = 'short_title', description='subtitle',
+    metadata='summary', heading='"Related events"',
+    short_text = 'short_title', description='summary',
     )
