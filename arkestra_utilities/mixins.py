ed98a6900121a47e97314aa937eb5dd8ea2b28c5
Daniele Procida <daniele@apple-juice.co.uk>
2011-10-26 08:15:40 +0100

    removed print statement

--- arkestra_utilities/mixins.py
+++ arkestra_utilities/mixins.py
@@ -23,14 +23,13 @@ class AutocompleteMixin(object):
         }    
 
     def formfield_for_dbfield(self, db_field, **kwargs):
-        print "def formfield_for_dbfield(self, db_field, **kwargs):"
         """
         Overrides the default widget for Foreignkey fields if they are
         specified in the related_search_fields class attribute.
         """
         if (isinstance(db_field, ForeignKey) and 
                 db_field.name in self.related_search_fields):
-            kwargs['widget'] = fk_lookup.FkLookup(db_field.rel.to)
+            kwargs['widget'] = fk_lookup.FkLookup(db_field.rel.to)          
         return super(AutocompleteMixin, self).formfield_for_dbfield(db_field, **kwargs)
 
 
