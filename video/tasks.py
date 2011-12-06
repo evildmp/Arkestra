09b0d57b1f7d4cd15c7862eede33cc654fb4597b
Daniele Procida <daniele@vurt.org>
2011-12-06 23:56:16 +0000

    various tweaks for the queue

--- video/tasks.py
+++ video/tasks.py
@@ -4,6 +4,11 @@ from models import VideoVersion
 
 @task
 def encodevideo(source, size, codec):
+    logger =  encodevideo.get_logger()
+    logger.info("**********************")
+    print "======== in encodevideo task ========="
     version, created = VideoVersion.objects.get_or_create(source = source, size = size, codec = codec)
-    version.encode()
-    return
+    print "== version, created", version, created
+    version_status = version.encode()
+    print "== version_status", version_status
+    return version_status
