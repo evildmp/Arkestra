4c287d78606bda228e715cd33914282e175306b4
Daniele Procida <daniele@vurt.org>
2012-05-09 21:03:39 +0100

    housekeeping urls

--- housekeeping/urls.py
+++ housekeeping/urls.py
@@ -2,9 +2,15 @@ from django.conf.urls.defaults import *
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    # first, try to match /housekeeping/<task>/<execute>
     (r"^housekeeping/statistics/", "housekeeping.statistics.stats"),
+
+    # /housekeeping/repair_mptt/contacts_and_people.Entity/
+    (r"^housekeeping/repair_mptt/(?P<slug>[-\w\.]+)/$", "housekeeping.repair_mptt.fix"),
+
+    # then, try to match /housekeeping/<task>/<execute>
     (r"^housekeeping/(?P<task>[^/]+)/(?P<action>[^/]+)/$", "housekeeping.tasks.tasks"),
+
+
     # # no match? 
     (r"^housekeeping/", "housekeeping.tasks.tasks"),
 
@@ -12,7 +18,6 @@ urlpatterns = patterns('',
     # (r"^housekeeping/clean_plugins/", "housekeeping.clean_plugins.clean"),
     # 
     # 
-    # (r"^repair_mptt/(?P<slug>[-\w\.]+)/$", "housekeeping.repair_mptt.fix"),
     
     # (r"^statistics/user/(?P<slug>[-\w]+)$", "housekeeping.statistics.userstats"),
     )    
