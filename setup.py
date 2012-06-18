12e82731777149ae7dcd55a27b3c5e93b55a20e1
Daniele Procida <daniele@vurt.org>
2012-06-19 00:11:01 +0100

    modified setup.py

--- setup.py
+++ setup.py
@@ -1,44 +1,44 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
     name='Arkestra',
     version='2.0.0',
     author='Daniele Procida',
     author_email='daniele@vurt.org',
-    packages=(
-        'arkestra_utilities',
-        'arkestra_image_plugin',
-        'contacts_and_people',
-        'housekeeping',
-        'links',
-        'news_and_events',
-        'vacancies_and_studentships',
-        # 'video',
+    packages=find_packages(),
+    # packages=(
+    #     'arkestra_utilities',
+    #     'arkestra_image_plugin',
+    #     'contacts_and_people',
+    #     'housekeeping',
+    #     'links',
+    #     'news_and_events',
+    #     'vacancies_and_studentships',
+    #     # 'video',
     ),
-    url='http://pypi.python.org/pypi/Arkestra/',
     license='LICENSE.txt',
     description='A semantic web publishing system for organisations',
-    long_description=open('README.txt').read(),
-    install_requires=[
-        "Django==1.3.1",            # Django, obviously
-        "django-cms==2.2",          # and django CMS
-        "South==0.7.5",             # for migrations
-        "django-mptt==0.5.1",       # necessary to force the right version
-        "django-typogrify==1.3",    # for HTML typography
-        "django-polymorphic",       # not sure what needs this
-        "django-appmedia",
-        
-        # for the semantic editor
-        "pyquery==1.1.1",
-        "lxml==2.3.4",
-        "ElementTree",
-        
-        "BeautifulSoup==3.2.1",     # for the template introsepction
-        
-        # for images
-        "PIL==1.1.7",               # never works properly 
-        "easy-thumbnails==1.0-alpha-21", # hooray for SmileyChris
-    ],
+    long_description=open(join(dirname(__file__), 'README.txt')).read(),
+    # requires=[
+    #     "Django==1.3.1",            # Django, obviously
+    #     "django-cms==2.2",          # and django CMS
+    #     "South==0.7.5",             # for migrations
+    #     "django-mptt==0.5.1",       # necessary to force the right version
+    #     "django-typogrify==1.3",    # for HTML typography
+    #     "django-polymorphic",       # not sure what needs this
+    #     "django-appmedia",
+    #     
+    #     # for the semantic editor
+    #     "pyquery==1.1.1",
+    #     "lxml==2.3.4",
+    #     "ElementTree",
+    #     
+    #     "BeautifulSoup==3.2.1",     # for the template introsepction
+    #     
+    #     # for images
+    #     "PIL==1.1.7",               # never works properly 
+    #     "easy-thumbnails==1.0-alpha-21", # hooray for SmileyChris
+    # ],
 )
  
 """
