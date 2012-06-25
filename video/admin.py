29239502726a144049cf6d453ff90be29656d3e7
Daniele Procida <daniele@vurt.org>
2012-06-25 16:09:10 +0100

    video admin/models tidy-up

--- video/admin.py
+++ video/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 # from django import forms
 # 
-# from django.utils.translation import ugettext  as _
+from django.utils.translation import ugettext  as _
 # 
 from filer.admin.fileadmin import FileAdmin
 # 
-from models import Video #, VideoVersion, ArkestraVideo
+from models import Video, VideoVersion
 #                                                           
 
 # class VideoVersionForm(forms.ModelForm):
@@ -17,34 +17,23 @@ from models import Video #, VideoVersion, ArkestraVideo
 #     class Meta:
 #         model = Video
 # 
-# class VideoAdmin(FileAdmin):
-#     form = VideoAdminChangeForm
-#     fieldsets = (
-#         (None, {
-#             'fields': ('name', 'owner','description')
-#         }),
-#         (None, {
-#             'fields': ('is_public',)
-#             
-#         }),
-#         (_('Advanced'), {
-#             'fields': ('file','sha1',),
-#             'classes': ('collapse',),
-#         }),
-#     )
-#     
-# 
-admin.site.register(Video, FileAdmin)
 
-# class VideoVersionInline(admin.TabularInline):
-#     # form = VideoVersionForm    
-#     model = VideoVersion
-#     readonly_fields = ('size', 'codec', 'status')
-#     extra = 0
-# 
-# 
-# class VideoVersionAdmin(admin.ModelAdmin):
-#     list_display = ('size', 'codec', 'status')
+class VideoVersionInline(admin.TabularInline):
+    # form = VideoVersionForm    
+    model = VideoVersion
+    readonly_fields = ('size', 'codec', 'status')
+    extra = 0
+
+
+class VideoAdmin(FileAdmin):
+    inlines = (VideoVersionInline,)
+    
+
+admin.site.register(Video, VideoAdmin)
+
+
+class VideoVersionAdmin(admin.ModelAdmin):
+    list_display = ('size', 'codec', 'status')
 # 
 # 
 # class MyVideoAdmin(VideoAdmin):
@@ -57,4 +46,4 @@ admin.site.register(Video, FileAdmin)
 # admin.site.unregister(Video)
 # admin.site.register(Video, MyVideoAdmin)
 # admin.site.register(ArkestraVideo, ArkestraVideoAdmin)
-# admin.site.register(VideoVersion, VideoVersionAdmin)
+admin.site.register(VideoVersion, VideoVersionAdmin)
