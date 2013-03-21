843e4e037b6828361f3840505e55a5b69b85e633
Daniele Procida <daniele@vurt.org>
2013-03-21 21:27:43 +0000

    setup.py

--- setup.py
+++ setup.py
@@ -13,13 +13,13 @@ setup(
     description='A semantic web publishing system for organisations',
     long_description=open(join(dirname(__file__), 'README.txt')).read(),
     install_requires=[
-    'django',
-    'django-typogrify', 
-    'BeautifulSoup',
-    'django-cms == 2.3.4',
-    'django-filer',
-    'semanticeditor',
-    'pillow',
+        'django >= 1.4, 1.4.5',
+        'django-typogrify', 
+        'BeautifulSoup',
+        'django-cms == 2.3.5',
+        'django-filer',
+        # 'semanticeditor',
+        'pillow',
     ]
 )
  
