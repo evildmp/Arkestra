d668913f3e9493789a1c78605da8cb9ce93b7d87
Daniele Procida <daniele@vurt.org>
2013-04-10 20:16:16 +0100

    removed example_14 project

--- example_14/example_14/legacy_finders.py
+++
@@ -1,8 +0,0 @@
-from django.contrib.staticfiles.storage import AppStaticStorage
-from django.contrib.staticfiles.finders import AppDirectoriesFinder
-
-class LegacyAppMediaStorage(AppStaticStorage):
-    source_dir = 'media'
-
-class LegacyAppDirectoriesFinder(AppDirectoriesFinder):
-    storage_class = LegacyAppMediaStorage
