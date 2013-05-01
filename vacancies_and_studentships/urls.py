a154ccdf9bb0d6ba06b4038789437bd98d656282
Daniele Procida <daniele@vurt.org>
2013-05-01 14:40:04 +0100

    fix incorrect url pattern for archived studentships

--- vacancies_and_studentships/urls.py
+++ vacancies_and_studentships/urls.py
@@ -10,7 +10,7 @@ urlpatterns = patterns('',
     # entities' vacancies and studentships
     url(r'^archived-vacancies/(?:(?P<slug>[-\w]+)/)?$', views.archived_vacancies, name="archived_vacancies"),
     url(r'^all-open-vacancies/(?:(?P<slug>[-\w]+)/)?$', views.all_current_vacancies, name="all_current_vacancies"),
-    url(r'^archived-studentships/(?:(?P<slug>[-\w]+)/)?$', views.vacancies_and_studentships, name="vacancies_and_studentships"),
+    url(r'^archived-studentships/(?:(?P<slug>[-\w]+)/)?$', views.archived_studentships, name="archived_studentships"),
     url(r"^all-open-studentships/(?:(?P<slug>[-\w]+)/)?$", views.all_current_studentships, name="all_current_studentships"),
     url(r"^vacancies-and-studentships/(?:(?P<slug>[-\w]+)/)?$", views.vacancies_and_studentships, name="vacancies_and_studentships"),
     )
