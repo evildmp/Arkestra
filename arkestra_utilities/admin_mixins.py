85fd8da203de77a490bf1202a3e83825e686a0f5
Daniele Procida <daniele@vurt.org>
2013-11-06 10:40:18 +0000

    PEP8 for admin_mixins

--- arkestra_utilities/admin_mixins.py
+++ arkestra_utilities/admin_mixins.py
@@ -1,5 +1,4 @@
 from django.db.models import ForeignKey
-from django.conf import settings
 from django import forms
 from django.contrib.admin import SimpleListFilter
 from django.utils.translation import ugettext_lazy as _
@@ -10,8 +9,8 @@ from widgetry import fk_lookup
 from widgetry.tabs.placeholderadmin import ModelAdminWithTabsAndCMSPlaceholder
 
 from contacts_and_people.models import Entity
-from links.models import ExternalLink
 from links.utils import get_or_create_external_link
+
 
 class AutocompleteMixin(object):
     class Media:
@@ -38,7 +37,9 @@ class AutocompleteMixin(object):
 
 class SupplyRequestMixin(object):
     def get_form(self, request, obj=None, **kwargs):
-        form_class = super(SupplyRequestMixin, self).get_form(request, obj, **kwargs)
+        form_class = super(SupplyRequestMixin, self).get_form(
+            request, obj, **kwargs
+            )
         form_class.request = request
         return form_class
 
@@ -62,37 +63,38 @@ class HostedByFilter(SimpleListFilter):
             return queryset.exclude(hosted_by__in=entities)
 
 fieldsets = {
-    'basic': ('', {'fields': ('title',  'short_title', 'summary')}),
-    'host': ('', {'fields': ('hosted_by',)}),
-    'image': ('', {'fields': ('image',)}),
-    'publishing_control': ('Publishing control', {'fields': ('published', 'in_lists')}),
-
-    'body':  ('', {
-        'fields': ('body',),
-        'classes': ('plugin-holder', 'plugin-holder-nopage',)
-        }),
-
-    'where_to_publish': ('', {'fields': ('publish_to',)}),
-
-    'people': ('People to contact about this item', {'fields': ('please_contact',)}),
-
-    'date': ('', {'fields': ('date',)}),
-    'importance': ('', {'fields': ('importance',)}),
-
-    'url': ('If this is an external item', {'fields': ('external_url', 'input_url',)}),
-    'slug': ('If this is an internal item', {'fields': ('slug',)}),
-
-    'location': ('', {'fields': ('precise_location', 'access_note',)}),
-    'address_report': ('', {'fields': ('address_report',)}),
-    'email': ('', {'fields': ('email',)}),
+    'basic': ['', {'fields': ['title',  'short_title', 'summary']}],
+    'host': ['', {'fields': ['hosted_by']}],
+    'image': ['', {'fields': ['image']}],
+    'publishing_control': [
+        'Publishing control', {'fields': ['published', 'in_lists']}
+        ],
+    'body':  ['', {
+        'fields': ['body'],
+        'classes': ['plugin-holder', 'plugin-holder-nopage'],
+        }],
+    'where_to_publish': ['', {'fields': ['publish_to']}],
+    'people': [
+        'People to contact about this item',
+        {'fields': ['please_contact']}
+        ],
+    'date': ['', {'fields': ['date']}],
+    'importance': ['', {'fields': ['importance']}],
+    'url': [
+        'If this is an external item',
+        {'fields': ['external_url', 'input_url']}
+        ],
+    'slug': ['If this is an internal item', {'fields': ['slug']}],
+    'location': ['', {'fields': ['precise_location', 'access_note']}],
+    'address_report': ['', {'fields': ['address_report']}],
+    'email': ['', {'fields': ['email']}],
     }
 
 
 class GenericModelAdmin(
-    AutocompleteMixin,
-    SupplyRequestMixin,
-    ModelAdminWithTabsAndCMSPlaceholder
-    ):
+        AutocompleteMixin, SupplyRequestMixin,
+        ModelAdminWithTabsAndCMSPlaceholder
+        ):
 
     tabs = (
         ['Basic', {
@@ -102,8 +104,7 @@ class GenericModelAdmin(
                 fieldsets["image"],
                 fieldsets["publishing_control"],
                 ),
-            }
-        ],
+            }],
         ['Body', {'fieldsets': [fieldsets["body"]]}],
         ['Where to Publish', {'fieldsets': [fieldsets["where_to_publish"]]}],
         ['Related people', {'fieldsets': [fieldsets["people"]]}],
@@ -124,7 +125,7 @@ class GenericModelAdmin(
     def formfield_for_manytomany(self, db_field, request, **kwargs):
         if db_field.name == "publish_to":
             kwargs["queryset"] = Entity.objects.filter(
-                website__published = True
+                website__published=True
                 )
         return super(AutocompleteMixin, self).formfield_for_manytomany(
             db_field, request, **kwargs
@@ -147,46 +148,57 @@ class InputURLMixin(forms.ModelForm):
     #           StudentshipForm(VacancyStudentshipForm)
     #
     # when https://code.djangoproject.com/ticket/19617 is fixed
-    # probably by https://github.com/linovia/django/compare/master...forms_metaclasses_rationalization
     # we can do something nicer
 
-    input_url = forms.CharField(max_length=255, required = False,
-        help_text=u"<strong>External URL</strong> not found above? Enter a new one.",
+    input_url = forms.CharField(
+        max_length=255, required=False,
+        help_text=u"""
+            <strong>External URL</strong> not found above? Enter a new one.""",
         )
+
 
 class GenericModelForm(InputURLMixin):
     class Meta:
-        widgets = {'summary': forms.Textarea(
-              attrs={'cols':80, 'rows':2,},
-            ),
-        }
+        widgets = {'summary': forms.Textarea(attrs={'cols': 80, 'rows': 2})}
 
     def clean(self):
         super(GenericModelForm, self).clean()
 
         # create the short_title automatically if necessary
-        if not self.cleaned_data["short_title"] and self.cleaned_data.get("title"):
+        if not self.cleaned_data["short_title"] and \
+                self.cleaned_data.get("title"):
             if len(self.cleaned_data["title"]) > 70:
-                raise forms.ValidationError("Please provide a short (less than 70 characters) version of the Title for the Short title field.")
+                raise forms.ValidationError("""
+                    Please provide a short (less than 70 characters) version
+                    of the Title for the Short title field.
+                    """)
             else:
                 self.cleaned_data["short_title"] = self.cleaned_data["title"]
 
         # check ExternalLink-related issues
-        self.cleaned_data["external_url"] = get_or_create_external_link(self.request,
-            self.cleaned_data.get("input_url", None), # a manually entered url
-            self.cleaned_data.get("external_url", None), # a url chosen with autocomplete
-            self.cleaned_data.get("title"), # link title
-            self.cleaned_data.get("summary"), # link description
+        self.cleaned_data["external_url"] = get_or_create_external_link(
+            self.request,
+            self.cleaned_data.get("input_url", None),  # a manually entered url
+            # a url chosen with autocomplete
+            self.cleaned_data.get("external_url", None),
+            self.cleaned_data.get("title"),  # link title
+            self.cleaned_data.get("summary"),  # link description
             )
 
         # misc checks
         if not self.cleaned_data["external_url"]:
             if not self.cleaned_data["hosted_by"]:
-                raise forms.ValidationError("A Host is required except for items on external websites - please provide either a Host or an External URL")
+                raise forms.ValidationError("""
+                    A Host is required except for items on external websites -
+                    please provide either a Host or an External URL
+                    """)
             # must have body or url in order to be published
             if not self.instance and self.instance.body.cmsplugin_set.all():
             # if not self.cleaned_data["body"]:
-                message = u"This will not be published until either an external URL or Plugin has been added. Perhaps you ought to do that now."
+                message = u"""
+                    This will not be published until either an external URL or
+                    Plugin has been added. Perhaps you ought to do that now.
+                    """
                 messages.add_message(self.request, messages.WARNING, message)
 
         return self.cleaned_data
